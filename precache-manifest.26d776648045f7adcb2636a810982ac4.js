e6c9b43c8a762b36c6cbab181de0b0f4fe2aa432
carlos zavala <cazcarlos@hotmail.com>
2020-04-30 20:10:34 -0500

    Updates

---
+++ precache-manifest.26d776648045f7adcb2636a810982ac4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40414a325d192b582f00d475d693b112",
+    "url": "/covidTrackerChallenge/index.html"
+  },
+  {
+    "revision": "ab40b5b5dbe34dde2d08",
+    "url": "/covidTrackerChallenge/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "2899c2a4255cf7df2a6a",
+    "url": "/covidTrackerChallenge/static/js/2.e7fcacb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidTrackerChallenge/static/js/2.e7fcacb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab40b5b5dbe34dde2d08",
+    "url": "/covidTrackerChallenge/static/js/main.cb8c6041.chunk.js"
+  },
+  {
+    "revision": "3239c88c98ab8cb934d8",
+    "url": "/covidTrackerChallenge/static/js/runtime-main.16926bbc.js"
+  },
+  {
+    "revision": "ef09b665abb281eea2c12e07e4b3a554",
+    "url": "/covidTrackerChallenge/static/media/covid1.ef09b665.jpg"
+  },
+  {
+    "revision": "44aeca093500882a91297e72251862de",
+    "url": "/covidTrackerChallenge/static/media/covid2.44aeca09.jpg"
+  },
+  {
+    "revision": "d049d7c9ed2176ceee9b8d761e1f60b0",
+    "url": "/covidTrackerChallenge/static/media/covid3.d049d7c9.jpg"
+  },
+  {
+    "revision": "156f9556a4b2a7c56ad3b1196af18fff",
+    "url": "/covidTrackerChallenge/static/media/covid4.156f9556.jpg"
+  },
+  {
+    "revision": "e32362411e7585404220a08d16ec5e9f",
+    "url": "/covidTrackerChallenge/static/media/covid5.e3236241.jpg"
+  },
+  {
+    "revision": "91b44ff06a4b912817796362aa0febf2",
+    "url": "/covidTrackerChallenge/static/media/covid6.91b44ff0.jpg"
+  }
+]);
